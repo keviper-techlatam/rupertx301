0f6c714d28cf289be18cf756336a3f984f825461
Kevin Perez <kevinalbertoperez@gmail.com>
2018-09-12 00:49:40 -0400

    Issue Migration Risk

--- db/migrate/20180912032049_create_risks.rb
+++ db/migrate/20180912032049_create_risks.rb
@@ -1,7 +1,7 @@
 class CreateRisks < ActiveRecord::Migration[5.2]
   def change
     create_table :risks do |t|
-      # t.references :account, foreign_key: true
+      t.references :account, foreign_key: true
       t.float :risk
 
       t.timestamps
